b6a543a473f61997dd00f07a4764e98eb9752571
sanskruti681 <sanskrutiekale111@gmail.com>
2025-11-16 22:14:58 +0530

    Add files via upload

---
+++ display the factorial of a number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int n;
+int fact=1;
+printf("Enter a number:");
+ scanf("%d",&n);
+    if(n<0)
+       printf("factorial is not defined for negative number.\n");
+else{
+for(int i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+ printf("factorial of %d=%d\n",n,fact);
+}
+return 0;
+}
