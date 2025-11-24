15d5adeb014712af944b36ddf9975b29523b7131
sanskruti681 <sanskrutiekale111@gmail.com>
2025-11-24 08:40:50 +0530

    Add files via upload

---
+++ program to define a structure and access it members.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct Student{
+int id;
+char name[20];
+float marks;
+};
+int main(){
+    struct Student s1={101,"priya",89.5};
+    struct Student s2={102,"tina",85.5};
+    struct Student s3={103,"chithra",90.5};
+    printf("ID: %d\n",s1.id);
+    printf("Name:%s\n",s1.name);
+    printf("Marks:%.2f\n",s1.marks);
+
+    printf("ID: %d\n",s2.id);
+    printf("Name:%s\n",s2.name);
+    printf("Marks:%.2f\n",s2.marks);
+
+     printf("ID: %d\n",s3.id);
+    printf("Name:%s\n",s3.name);
+    printf("Marks:%.2f\n",s3.marks);
+    return 0;
+}
