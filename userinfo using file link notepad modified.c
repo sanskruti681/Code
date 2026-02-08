7bec5b04333dc08f7409a01d7d4bdc2f9b7e1aa6
sanskruti681 <sanskrutiekale111@gmail.com>
2026-02-08 14:50:10 +0530

    Add files via upload

---
+++ userinfo using file link notepad modified.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    char path[200]= "start C:\\Users\\abhay\\OneDrive\\Desktop\\userinfoo.txt";
+    printf(" opening ....... \n");
+    system(path);
+
+    return 0 ;
+
+
+}
